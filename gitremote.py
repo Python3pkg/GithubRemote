383f7135365bc5a06c93117c2af43d2364807c5c
Cameron White <cawhite@pdx.edu>
2013-11-25 03:52:17 -0800

    Added main [BUGS]
    Something is wrong with the JSON message format

--- gitremote.py
+++ gitremote.py
@@ -3,6 +3,10 @@ from github import MainClass
 from github.Requester import Requester
 import re
 import argparse
+import json
+
+class Require2FAError(Exception):
+    pass
 
 PARSER = argparse.ArgumentParser(description="Control remote git repos")
 
@@ -13,12 +17,6 @@ PARSER.add_argument('host', choices=['github'], type=str)
 SUBPARSER = PARSER.add_subparsers()
 
 SUBPARSER_LIST = SUBPARSER.add_parser('list')
-
-if __name__ == '__main__':
-    ARGS = PARSER.parse_args()
-
-class Require2FAError(Exception):
-    pass
 
 def request_token(username, password, 
         scopes, user_agent, code_2fa=None,
@@ -32,11 +30,24 @@ def request_token(username, password,
     request_header = None
     if code_2fa:
         request_header = {'x-github-otp': code_2fa}
+    s = '{{"scopes": ["{}"], "note": "{}"}}'.format(scopes, user_agent)
     status, header, message = requester.requestJson(
             "POST", "/authorizations", 
-            {"scopes":scopes, "note": str(user_agent)},
+            json.loads(s),
             request_header)
     if status == 401 and re.match(r'.*required.*', header['x-github-otp']):
         raise Require2FAError()
     else:
-        return message
+        return status, header, message
+
+if __name__ == '__main__':
+    ARGS = PARSER.parse_args()
+    try:
+        resp = request_token(ARGS.username, ARGS.password,
+                'repos', 'TESTTEST')
+    except Require2FAError:
+        code = input("Enter code: ")
+        resp = request_token(ARGS.username, ARGS.password,
+                'repos', 'TESTTEST', code)
+    print(resp)
+        
