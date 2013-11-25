caaf8793a5a4319ddb814abb97f3acf3bcd59167
Cameron White <cawhite@pdx.edu>
2013-11-25 03:12:20 -0800

    Added request_token function
    two-factor authentication is supported. The code still needs to
    be clean up but everything seams to be working.

---
+++ gitremote.py
@@ -0,0 +1,29 @@
+from github import Github
+from github import MainClass
+from github.Requester import Requester
+import re
+
+class Require2FAError(Exception):
+    pass
+
+def request_token(username, password, 
+        scopes, user_agent, code_2fa=None,
+        base_url=MainClass.DEFAULT_BASE_URL, 
+        timeout=MainClass.DEFAULT_TIMEOUT, 
+        client_id=None,
+        client_secret=None,
+        per_page=MainClass.DEFAULT_PER_PAGE):
+    requester = Requester(username, password, base_url, timeout,
+            client_id, client_secret, user_agent, per_page)
+    request_header = None
+    if code_2fa:
+        request_header = {'x-github-otp': code_2fa}
+    status, header, message = requester.requestJson(
+            "POST", "/authorizations", 
+            {"scopes":scopes, "note": str(user_agent)},
+            request_header)
+    if status == 401 and re.match(r'.*required.*', header['x-github-otp']):
+        raise Require2FAError()
+    else:
+        return message
+
