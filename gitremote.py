8873d6e54d64d81d22bfe7d1ba00c2e2139918bc
Cameron White <cawhite@pdx.edu>
2013-11-26 02:04:15 -0800

    File structure

--- gitremote.py
+++ gitremote.py
@@ -1,14 +1,11 @@
-from github import Github
-from github import MainClass
-from github.Requester import Requester
-import re
-import argparse
+#!/usr/bin/env python
+# -*- coding: utf-9 -*-
+# Copyright (C) 2013, Cameron White
+from core import *
 import json
 import getpass
 import os
 
-class Require2FAError(Exception):
-    pass
 
 PARSER = argparse.ArgumentParser(description="Control remote git repos")
 
@@ -20,30 +17,6 @@ SUBPARSER = PARSER.add_subparsers()
 
 SUBPARSER_LIST = SUBPARSER.add_parser('list')
 
-def request_token(
-        username, password, scopes, user_agent, code_2fa=None,
-        base_url=MainClass.DEFAULT_BASE_URL, 
-        timeout=MainClass.DEFAULT_TIMEOUT, 
-        client_id=None, client_secret=None,
-        per_page=MainClass.DEFAULT_PER_PAGE):
-    
-    requester = Requester(username, password, base_url, timeout,
-            client_id, client_secret, user_agent, per_page)
-
-    if code_2fa:
-        request_header = {'x-github-otp': code_2fa}
-    else:
-        request_header = None
-
-    status, header, message = requester.requestJson(
-            "POST", "/authorizations", 
-            input={"scopes": scopes, "note": str(user_agent)},
-            headers=request_header)
-
-    if status == 401 and re.match(r'.*required.*', header['x-github-otp']):
-        raise Require2FAError()
-    else:
-        return status, header, message
 
 if __name__ == '__main__':
 
