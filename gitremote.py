1666f5b97a90561ae67b6eb68cafe10cf990b49b
Cameron White <cawhite@pdx.edu>
2013-11-27 20:56:46 -0800

    Changed the way the command line authenticates

--- gitremote.py
+++ gitremote.py
@@ -1,63 +1,57 @@
 #!/usr/bin/env python
-# -*- coding: utf-9 -*-
+# -*- coding: utf-8 -*-
 # Copyright (C) 2013, Cameron White
 from core import *
 import json
 import getpass
 import os
 
-
 PARSER = argparse.ArgumentParser(description="Control remote git repos")
 
-PARSER.add_argument('--username', '-u', type=str)
-PARSER.add_argument('--password', '-p', type=str)
+PARSER.add_argument('--username', '-u', default=None, type=str)
+PARSER.add_argument('--password', '-p', default=None, type=str)
 PARSER.add_argument('host', choices=['github'], type=str)
 
 SUBPARSER = PARSER.add_subparsers()
 
 SUBPARSER_LIST = SUBPARSER.add_parser('list')
 
+ARGS = PARSER.parse_args()
 
-if __name__ == '__main__':
-
-    ARGS = PARSER.parse_args()
-
-    if not ARGS.username:
-        ARGS.username = input("username: ")
-    
-    if os.path.exists('./loggins.json'):
-        loggin_info = open('./loggins.json', 'r+')    
-    else:
-        loggin_info = open('./loggins.json', 'w+')    
-    
+def get_token():
     try:
-        loggin_info_json = json.load(loggin_info)
-    except ValueError:
-        loggin_info_json = json.loads('{}')
-    
+        f = open('authentication.pickle', 'r+')
+    except IOError:
+        f = open('authentication.pickle', 'w+')
+
     try:
-        token = loggin_info_json[ARGS.host][ARGS.username]['token']
-    except KeyError:
+        authentication = pickle.load(f)
+    except EOFError:
+
+        if not ARGS.username:
+            ARGS.username = raw_input("username: ")
 
         if not ARGS.password:
             ARGS.password = getpass.getpass()
 
         try:
-            status, header, message = request_token(
+            authenticaiton = request_token(
                     ARGS.username, ARGS.password, ['repo'], 'TESTTEST')
-
         except Require2FAError:
-            code = input("Enter code: ")
-            status, header, message = request_token(
+            pass
+    
+        try:
+            code = raw_input("Enter code: ")
+            authentication = request_token(
                     ARGS.username, ARGS.password, ['repo'], 'TESTTEST', code)
 
-            message_json = json.loads(message)
-          
-            if not ARGS.host in loggin_info_json:
-                loggin_info_json[ARGS.host] = {}
-            if not ARGS.username in loggin_info_json[ARGS.host]:
-                loggin_info_json[ARGS.host][ARGS.username] = {}
+            pickle.dump(authentication, f)
+        except None:
+            return None
 
-            loggin_info_json[ARGS.host][ARGS.username]['token'] = message_json['token']
+        return authentication
 
-    json.dump(loggin_info_json,loggin_info)
+if __name__ == '__main__':
+
+    get_token()
+
