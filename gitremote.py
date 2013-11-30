5d473d425601e2fd206cfc36f86c9afce110bbb2
Cameron White <cawhite@pdx.edu>
2013-11-29 22:59:47 -0800

    Now non 2fa works

--- gitremote.py
+++ gitremote.py
@@ -39,7 +39,7 @@ def get_token():
             authenticaiton = request_token(
                     ARGS.username, ARGS.password, ['repo'], 'TESTTEST')
         except Require2FAError:
-            pass
+            return authentication
     
         try:
             code = raw_input("Enter code: ")
